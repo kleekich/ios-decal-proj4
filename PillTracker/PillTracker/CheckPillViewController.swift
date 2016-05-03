78eda1a1e79a292909e81aabdecf99e7631c4dae
kleekich <kleekich@berkeley.edu>
2016-05-02 19:29:10 -0700

    UI added for checking, CheckPillViewController added

---
+++ PillTracker/PillTracker/CheckPillViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CheckPillViewController.swift
+//  PillTracker
+//
+//  Created by Lee Kangsik Kevin on 5/2/16.
+//  Copyright © 2016 Lee Kangsik Kevin. All rights reserved.
+//
+
+import Cocoa
+import UIKit
+
+
+class CheckPillViewController: UIViewController {
+
+}
